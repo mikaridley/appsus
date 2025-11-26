57d150b30e8f2987ab99f8f9fe2044e8f5f86ef7
Mika Ridley <mikaridley26@gmail.com>
2025-11-26 18:58:49 +0200

    Add openNote setup

--- apps/note/cmps/OpenNote.jsx
+++ apps/note/cmps/OpenNote.jsx
@@ -1,16 +1,30 @@
+import { noteService } from '../services/note.service.js'
+
 const { useParams, useNavigate, Link } = ReactRouterDOM
+const { useState, useEffect } = React
 
 export function OpenNote() {
   const { noteId } = useParams()
+  const [note, setNote] = useState(null)
   const navigate = useNavigate()
+
+  useEffect(() => {
+    loadNote()
+  }, [noteId])
+
+  function loadNote() {
+    noteService.get(noteId).then(setNote).catch(console.log)
+  }
 
   function onBack() {
     navigate('/note')
   }
+
+  if (!note) return <div>Loading...</div>
   return (
     <div onClick={onBack} className="note-black-screen">
       <div onClick={ev => ev.stopPropagation()} className="open-note">
-        <h1>hi</h1>
+        <h1>{note.id}</h1>
       </div>
     </div>
   )
