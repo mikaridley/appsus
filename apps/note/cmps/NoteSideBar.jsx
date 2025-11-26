cd9546b0afe7607116887c854cadb83188ee2600
Mika Ridley <mikaridley26@gmail.com>
2025-11-26 22:22:29 +0200

    Style side bar

--- apps/note/cmps/NoteSideBar.jsx
+++ apps/note/cmps/NoteSideBar.jsx
@@ -1,7 +1,27 @@
+const { useParams, useNavigate, Link } = ReactRouterDOM
+const { useState, useEffect } = React
+
 export function NoteSideBar() {
+  const navigate = useNavigate()
+  const [activePage, setActivePage] = useState('notes')
+
+  function onNotePage() {
+    navigate('/note')
+    setActivePage(page => 'notes')
+  }
   return (
     <section className="note-side-bar">
-      <div>side bar</div>
+      <div
+        onClick={onNotePage}
+        className={`side-bar-row ${activePage === 'notes' ? 'active' : ''}`}
+      >
+        <img src="assets/img/note/Default.png" />
+        <p>Notes</p>
+      </div>
+      <div className="side-bar-row">
+        <img src="assets/img/note/Default.png" />
+        <p>Bin</p>
+      </div>
     </section>
   )
 }
