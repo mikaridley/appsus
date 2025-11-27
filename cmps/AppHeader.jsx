6abe6f0fa6e3b6bf400674c2e0b194068db471ab
Mika Ridley <mikaridley26@gmail.com>
2025-11-27 17:25:37 +0200

    Make sidebar small

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,16 +1,29 @@
-const { Link, NavLink } = ReactRouterDOM
+const { Link, NavLink, useParams, useLocation } = ReactRouterDOM
+const { useState, useEffect } = React
 
 export function AppHeader() {
+  const { pathname } = useLocation()
 
-    return <header className="app-header">
-        <Link to="/">
-            <h3>LOGO!</h3>
-        </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
+  function toggleMenu() {
+    if (pathname === '/note') {
+      document.body.classList.toggle('note-menu-open')
+    }
+  }
+
+  return (
+    <header className="app-header">
+      <div onClick={toggleMenu} className="burger-container">
+        <img className="burger" src="assets/img/note/burger.svg" />
+      </div>
+      <Link to="/">
+        <h3>LOGO!</h3>
+      </Link>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/mail">Mail</NavLink>
+        <NavLink to="/note">Note</NavLink>
+      </nav>
     </header>
+  )
 }
